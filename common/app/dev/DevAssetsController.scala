4788bca8da99dcbf67949fd08f6168628757ee13
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-02-26 16:28:54 +0000

    Add method to get public static asset to dev controller

--- common/app/dev/DevAssetsController.scala
+++ common/app/dev/DevAssetsController.scala
@@ -22,6 +22,20 @@ object DevAssetsController extends Controller with ExecutionContexts {
     )
   }
 
+  def fromPublicRoot(path: String): Action[AnyContent] = Action {
+    val contentType: Option[String] = MimeTypes.forFileName(path) map { mime =>
+      // Add charset for text types
+      if (MimeTypes.isText(mime)) s"${mime}; charset=utf-8" else mime
+    }
+
+    val resolved = new File(s"static/public/$path").toURI.toURL
+
+    Result(
+      ResponseHeader(OK, Map(CONTENT_TYPE -> contentType.getOrElse(BINARY))),
+      Enumerator.fromStream(resolved.openStream())
+    )
+  }
+
   def atFonts(path: String) = at(s"fonts/$path")
   def atJavascripts(file: String) = at(s"javascripts/$file")
   def atStylesheets(file: String) = at(s"stylesheets/$file")
