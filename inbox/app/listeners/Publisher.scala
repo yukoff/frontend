3b23aa0b6cb73590a4f89a57fd8c3b5fb4657487
Robert Berry <robert.berry@theguardian.com>
2015-02-26 17:32:21 +0000

    Add publish push notification

--- inbox/app/listeners/Publisher.scala
+++ inbox/app/listeners/Publisher.scala
@@ -1,9 +1,18 @@
 package listeners
 
 import common.ExecutionContexts
-import models.{PushSubscription, Feed, Subscription, InboxItem}
+import models._
+import play.api.libs.json.Json
+import play.api.libs.ws.WS
 
 object Publisher extends ExecutionContexts {
+  def sendPushNotification(endpoint: PushEndpoint) = {
+    WS.url(endpoint.endpoint).post(Json.obj(
+      "registration_id" -> endpoint.userId,
+      "data.data" -> "{}"
+    ))
+  }
+
   def publish(topic: String, item: InboxItem) = {
     Subscription.getSubscriptions(topic) map { subscriptions =>
       for {
@@ -12,11 +21,8 @@ object Publisher extends ExecutionContexts {
         Feed.addPost(subscription, item)
 
         PushSubscription.getEndpoint(subscription) map { endpoint =>
-          // Send push notification!
-
-
+          endpoint.foreach(sendPushNotification)
         }
-
       }
     }
   }
