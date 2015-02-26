177379039dca453c3418be537fdb899fdda5e707
Robert Berry <robert.berry@theguardian.com>
2015-02-26 15:11:31 +0000

    Add CAPI events skeleton

--- inbox/app/listeners/ContentApiListener.scala
+++ inbox/app/listeners/ContentApiListener.scala
@@ -2,39 +2,74 @@ package listeners
 
 import akka.actor.Actor
 import akka.actor.Actor.Receive
+import common.Logging
 import conf.LiveContentApi
 import listeners.ContentApiListener.Initialise
 import com.gu.util.liveblogs.{KeyEvent, Parser}
+import scala.concurrent.duration._
 
 object ContentApiListener {
   case class Initialise(articlesSeen: Set[String], keyEventsSeen: Map[String, Set[String]])
+
+  case class Update(articlesSeen: Set[String], keyEventsSeen: Map[String, Set[String]])
+
+  case class UpdateError(error: Throwable)
 }
 
-class ContentApiListener extends Actor {
-  /** These would actually be stored in a database or something ... */
-  var articlesSeen = Set.empty[String]
-  var keyEventsSeen = Map.empty[String, Set[String]]
+class ContentApiListener extends Actor with Logging {
+  import ContentApiListener._
 
+  /** These would actually be stored in a database or something ... */
   val PageSize = 20
-/*
-  LiveContentApi.getResponse(
+
+  def getArticleIdsAndKeyEvents = LiveContentApi.getResponse(
     LiveContentApi.item("/", "uk").pageSize(PageSize)
   ) map { response =>
     val articlesSeen = response.results.map(_.id)
-    val keyEventsSeen = response.results.filter(_.tags.exists(_.id == "tone/minutebyminute")) map { liveBlog =>
-      liveBlog.id -> liveBlog.safeFields.get("body") map { body =>
-        Parser.parse(body).filter(_.postType == KeyEvent).map(_.id)
-      } getOrElse Seq.empty
-    }
+    val keyEventsSeen = (response.results.filter(_.tags.exists(_.id == "tone/minutebyminute")) map { liveBlog =>
+      liveBlog.id -> (liveBlog.safeFields.get("body") map { body: String =>
+        Parser.parse(body).filter(_.postType == KeyEvent).map(_.id).toSet
+      } getOrElse Set.empty)
+    }).toMap
 
-    self ! Initialise(articlesSeen.toSet, keyEventsSeen)
+    (articlesSeen.toSet, keyEventsSeen)
   }
 
-*/
+  getArticleIdsAndKeyEvents map { case (articlesSeen, keyEventsSeen) =>
+    self ! Initialise(articlesSeen, keyEventsSeen)
+  }
+
 
   override def receive: Receive = {
-    case _ => ???
+    case Initialise(articlesSeen, keyEventsSeen) => context.become(withState(articlesSeen, keyEventsSeen))
   }
 
+  def withState(articlesSeen: Set[String], keyEventsSeen: Map[String, Set[String]]): Receive = {
+    context.system.scheduler.scheduleOnce(10.seconds) {
+      log.info("Updating state from Content API")
 
+      getArticleIdsAndKeyEvents map { case (articlesSeen, keyEventsSeen) =>
+        self ! Update(articlesSeen, keyEventsSeen)
+      }
+    }
+
+    {
+      case Update(newArticlesSeen, newKeyEventsSeen) =>
+        val articleNotifications = newArticlesSeen -- articlesSeen
+
+        // TODO send these
+        if (articleNotifications.nonEmpty) {
+          log.info(s"New articles: $articleNotifications")
+        }
+
+        // TODO also diff the key events and send notifications for those
+
+
+        context.become(withState(newArticlesSeen, newKeyEventsSeen))
+
+      case UpdateError(error) =>
+        log.error("Error updating state from Content API", error)
+        context.become(withState(articlesSeen, keyEventsSeen))
+    }
+  }
 }
