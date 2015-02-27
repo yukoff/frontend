807df2b51c2b370d8644a9bc3ec5b84a7cc27a99
James A Gorrie <james.gorrie@theguardian.com>
2015-02-27 13:54:55 +0000

    fetch all the URLs

--- static/public/lofi.js
+++ static/public/lofi.js
@@ -1,42 +1,39 @@
-window.addEventListener('online',  online);
-window.addEventListener('offline', offline);
-//offline();
-function online() {
-    document.querySelector('html').classList.remove('offline');
-}
+window.fetch('/lolist').then(function(resp) {
+    resp.json().then(function(json) {
+        var urls = json.urls.map(function(url) {
+            var a = url.split('/');
+            var headline = capitalizeFirstLetter(a[a.length-1].replace(/-/g, ' '));
 
-function offline() {
-    document.querySelector('html').classList.add('offline');
-    window.fetch('/lolist').then(function(resp) {
-        resp.json().then(function(json) {
-            var urls = json.urls.map(function(url) {
-                var a = url.split('/');
-                var headline = capitalizeFirstLetter(a[a.length-1].replace(/-/g, ' '));
+            return { headline: headline, url: url };
+        });
 
-                return { headline: headline, url: url };
-            });
+        var links = urls.map(function(link) {
+            var a = document.createElement('a');
+            a.href = link.url;
+            a.innerHTML = link.headline;
 
-            var links = urls.map(function(link) {
-                var a = document.createElement('a');
-                a.href = link.url;
-                a.innerHTML = link.headline;
+            return a;
+        });
 
-                return a;
-            });
-
-            window.addEventListener('DOMContentLoaded', function() {
-                console.log('=load');
-                var lofilist = document.getElementById('lofi-list');
-                links.forEach(function(link) {
-                    console.log(link)
-                    lofilist.appendChild(link);
-                });
+        window.addEventListener('DOMContentLoaded', function() {
+            var lofilist = document.getElementById('lofi-list');
+            links.forEach(function(link) {
+                lofilist.appendChild(link);
             });
         });
-    }).catch(function() {});
-}
-
+    });
+}).catch(function() {});
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+window.fetch('/inbox/rob_test_3').then(function(resp) {
+    resp.json().then(function(json) {
+        json.messages.forEach(function(m) {
+            var u = m.message.url;
+            if(!u) { return; }
+            window.fetch('/lofi'+ u);
+        });
+    });
+});
