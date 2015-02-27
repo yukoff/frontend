2034aa33f611771bedcb1cb02088880c371e5fdc
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-02-27 14:33:47 +0000

    Validation

--- static/src/javascripts/projects/common/modules/climate/did-u-know.js
+++ static/src/javascripts/projects/common/modules/climate/did-u-know.js
@@ -12,24 +12,24 @@ define([
 
     var list = [
         {
-            title: "Missed the tube? Walk!",
-            tip: "It emits no pollution and is totally carbon free."
+            title: 'Missed the tube? Walk!',
+            tip: 'It emits no pollution and is totally carbon free.'
         },
         {
-            title: "Fine tune your computer settings",
-            tip: "65 per cent of energy used by computers is spent on running idle."
+            title: 'Fine tune your computer settings',
+            tip: '65 per cent of energy used by computers is spent on running idle.'
         },
         {
-            title: "Save water, save electricity",
-            tip: "Over 30 million litres of water are boiled in the UK every day only to go cold again."
+            title: 'Save water, save electricity',
+            tip: 'Over 30 million litres of water are boiled in the UK every day only to go cold again.'
         },
         {
-            title: "Eat seasonably and locally",
-            tip: "Slim down your carbon footprint and stop eating things that need to travel across the world. Like tomatoes in winter."
+            title: 'Eat seasonably and locally',
+            tip: 'Slim down your carbon footprint and stop eating things that need to travel across the world. Like tomatoes in winter.'
         },
         {
-            title: "Your impact on climate change",
-            tip: "You can set yourself your own carbon savings goal and then strive to achieve it."
+            title: 'Your impact on climate change',
+            tip: 'You can set yourself your own carbon savings goal and then strive to achieve it.'
         }
     ];
 
@@ -43,8 +43,8 @@ define([
                 '<a class="diduknow__btn" href="" title=""></a>' +
                 '<a class="diduknow__link" href="" title="">Learn more about climate change</a> ' +
                 '</div>');
-            $('.diduknow').css('top', (Math.random() * 2000) + 900 + 'px').toggleClass('u-h')
+            $('.diduknow').css('top', (Math.random() * 2000) + 900 + 'px').toggleClass('u-h');
 
         }
-    }
+    };
 });
