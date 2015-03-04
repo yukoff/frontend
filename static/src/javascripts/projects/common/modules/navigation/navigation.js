2e3a28e54b30379c8fbb3630eb041505a494e063
Alex Sanders <a@snd.rs>
2015-03-04 15:40:12 +0000

    old IE can't interrogate the innerText of a noscript

--- static/src/javascripts/projects/common/modules/navigation/navigation.js
+++ static/src/javascripts/projects/common/modules/navigation/navigation.js
@@ -18,40 +18,7 @@ define([
     Navigation.prototype.init = function () {
         this.enableMegaNavToggle();
         this.replaceAllSectionsLink();
-    };
-
-    Navigation.prototype.forageForMegaNav = function () {
-        return $('.js-navigation-placeholder', "[data-has-nav]").html() ||
-            $('#mega-nav-src')[0].innerText;
-    };
-
-    Navigation.prototype.regurgitateMegaNavInto = function ($nav) {
-        $('.js-navigation-placeholder', $nav[0]).html(this.forageForMegaNav());
-        $nav.attr('data-has-nav', 'true');
-        this.announceTheNewNavOnce();
-    };
-
-    Navigation.prototype.announceTheNewNavOnce = _.once(function() {
-        mediator.emit('modules:nav:inserted');
-    })
-
-    Navigation.prototype.replaceAllSectionsLink = function () {
-        $('.js-navigation-toggle').attr('href', '#nav-allsections');
-    };
-
-    Navigation.prototype.toggleMegaNav = function ($toggler) {
-        var that = this,
-            $nav = $('.' + $toggler.attr('data-target-nav'));
-
-        if ($nav.attr('data-has-nav') !== 'true') {
-            fastdom.write(function () {
-                that.regurgitateMegaNavInto($nav);
-            })
-        }
-
-        fastdom.write(function () {
-            $nav.toggleClass('navigation--expanded navigation--collapsed');
-        })
+        this.megaNavSrc = null;
     };
 
     Navigation.prototype.enableMegaNavToggle = function () {
@@ -61,6 +28,50 @@ define([
             e.preventDefault();
             that.toggleMegaNav($(e.currentTarget));
         });
+    };
+
+    Navigation.prototype.toggleMegaNav = function ($toggler) {
+        var that = this,
+            $nav = $('.' + $toggler.attr('data-target-nav'));
+
+        fastdom.write(function () {
+            that.regurgitateMegaNavInto($nav);
+            $nav.toggleClass('navigation--expanded navigation--collapsed');
+        })
+    };
+
+    Navigation.prototype.regurgitateMegaNavInto = function ($nav) {
+        var that = this;
+
+        if ($nav.attr('data-is-navigationable') === 'true') {
+            return;
+        }
+
+        fastdom.write(function () {
+            $('.js-navigation-placeholder', $nav[0]).html(that.forageForMegaNav());
+            $nav.attr('data-is-navigationable', 'true');
+            that.announceTheNewNavOnce();
+        })
+    };
+
+    Navigation.prototype.forageForMegaNav = function () {
+        return this.megaNavSrc || (this.megaNavSrc = function () {
+            var srcEl = document.getElementById('mega-nav-src');
+
+            if (srcEl) {
+                return srcEl[srcEl.nodeName === 'NOSCRIPT' ? 'textContent' : 'innerHTML'];
+            };
+
+            return null;
+        }());
+    };
+
+    Navigation.prototype.announceTheNewNavOnce = _.once(function() {
+        mediator.emit('modules:nav:inserted');
+    });
+
+    Navigation.prototype.replaceAllSectionsLink = function () {
+        $('.js-navigation-toggle').attr('href', '#nav-allsections');
     };
 
     return Navigation;
