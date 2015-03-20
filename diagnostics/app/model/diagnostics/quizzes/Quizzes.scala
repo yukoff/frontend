391752f3fefb3466decdd9509c37cbfe73e26aef
Justin Pinner <justinpinner@gmail.com>
2015-03-20 11:52:42 +0000

    Re-thinking the updates. Also still need memcached codec!

--- diagnostics/app/model/diagnostics/quizzes/Quizzes.scala
+++ diagnostics/app/model/diagnostics/quizzes/Quizzes.scala
@@ -7,51 +7,49 @@ import conf.Configuration
 import org.json4s.native.JsonMethods._
 import play.api.Logger
 import shade.memcached.{Configuration => MemcachedConf, Codec, Memcached}
-import org.joda.time.DateTime
 import org.json4s.{DefaultFormats, Extraction}
 
 import scala.concurrent.Future
+import scala.concurrent.duration.Duration
 
-object Quizzes extends ExecutionContexts {
+trait JsonImplicits {
   implicit val formats = new DefaultFormats {
     override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
   } ++ org.json4s.ext.JodaTimeSerializers.all
+}
 
-
+object Quizzes extends ExecutionContexts with JsonImplicits {
   lazy val host = Configuration.memcached.host.head
   lazy val memcached = Memcached(MemcachedConf(host), memcachedExecutionContext)
 
   def update(json: String) = {
     val quizUpdate = parse(json).extract[QuizUpdate]
-    val oldStats = results(quizUpdate.quizId)
+    val oldStats = aggregatedResults(quizUpdate.quizId)
 
-    val x = for {
+    for {
       stat <- oldStats
     } yield {
-      addLatest(stat, quizUpdate)
-    }
-    //memcached.set[QuizUpdate](quizUpdate.quizId, quizUpdate, )
-  }
-
-  private def addLatest(agg: QuizAggregate, upd: QuizUpdate) = {
-    agg.copy()
-  }
-
-  private def addResultsToList(aggList: List[List[Int]], newVals: List[Int]) = {
-    val extendedAgg = aggList ++ Stream.continually(Nil)
-    aggList.zip(newVals).map{ x => incrementByIndex(x._1, x._2) }
-  }
-
-  def incrementByIndex(a: List[Int], b: Int): List[Int] = {
-    // a mght be too short
-    (a, b) match {
-      case (Nil, 0) => List(1)
-      case (x::xs, 0) => x + 1 :: xs
-      case (x::xs, n) => x :: incrementByIndex(xs, b - 1)
+      val newResults = quizUpdate.questions.flatMap{ question =>
+        if (stat.results.nonEmpty) {
+          stat.results.collect{
+            case oldResult if oldResult.questionNum == question.number && oldResult.answerNum == question.response =>
+              AggregatedResponse(oldResult.questionNum, oldResult.answerNum, oldResult.answerCount + 1)
+            case oldResult => oldResult
+          }
+        } else {
+          // if this is the first time we've ever seen results for this quiz, we need to know the full picture
+          // of the possible responses so we can compile the aggregate properly. Currently the only way to find that
+          // out is if the json in the update from the browser supplies it all the time.
+          List(AggregatedResponse(question.number, question.response, 1L))  // this is insufficient!
+        }
+      }
+      val newAggregate = stat.copy(results = newResults)
+      // todo: Duration.Undefined??
+      memcached.set[QuizAggregate](quizUpdate.quizId, newAggregate, Duration.Undefined)
     }
   }
 
-  def results(quizId: String): Future[QuizAggregate] = {
+  def aggregatedResults(quizId: String): Future[QuizAggregate] = {
     memcached.get[QuizAggregate](quizId).recover {
       case e: Exception =>
         Logger.error(e.getMessage)
@@ -62,19 +60,20 @@ object Quizzes extends ExecutionContexts {
 
 case class QuizUpdate (
   quizId: String,
-  results: List[Int],
-  timeTaken: Long)
+  questions: List[QuizQuestion],
+  timeSeconds: Long) extends JsonImplicits
 {
   def toJson = Extraction.decompose(this)
 }
 
+case class QuizQuestion(number: Int, response: Int)
+
 case class QuizAggregate (
   quizId: String,
-  results: List[List[Int]],
-  timeTaken: Long)
+  results: List[AggregatedResponse],
+  timeSeconds: Long) extends JsonImplicits
 {
   def toJson = Extraction.decompose(this)
-  def isCorrect(q: Int) = {
-    results(q) == 1
-  }
 }
+
+case class AggregatedResponse(questionNum: Int, answerNum: Int, answerCount: Long)
