58c29c2d7ad00ae5cb8659eb9b737cdb0e4d91e4
Alex Sanders <a@snd.rs>
2015-04-13 09:13:59 +0100

    do nothing if the sticky element has native position:sticky

--- static/src/javascripts/projects/common/modules/ui/sticky.js
+++ static/src/javascripts/projects/common/modules/ui/sticky.js
@@ -9,53 +9,57 @@ define([
     mediator,
     fastdom
 ) {
+    var Sticky, $element = bonzo(element);
 
-    /**
-     * @todo: check if browser natively supports "position: sticky"
-     */
-    var Sticky = function (element, options) {
-        this.$element = bonzo(element);
-        this.$parent  = this.$element.parent();
-        this.opts     = _.defaults(options || {}, {
-            top: 0
-        });
-        this.read = null;
-        this.write = null;
-    };
+    if (/sticky/.test(window.getComputedStyle($element).getPropertyValue("position"))) {
+        // we have native support but let's not break the API...
+        Sticky = function () {};
+        Sticky.prototype.init = function () {};
+    } else {
+        Sticky = function (element, options) {
+            this.$element = $element;
+            this.$parent  = this.$element.parent();
+            this.opts     = _.defaults(options || {}, {
+                top: 0
+            });
+            this.read = null;
+            this.write = null;
+        };
 
-    Sticky.prototype.init = function () {
-        mediator.on('window:scroll', this.updatePosition.bind(this));
-        // kick off an initial position update
-        this.updatePosition();
-    };
+        Sticky.prototype.init = function () {
+            mediator.on('window:scroll', this.updatePosition.bind(this));
+            // kick off an initial position update
+            this.updatePosition();
+        };
 
-    Sticky.prototype.updatePosition = function () {
-        var fixedTop, css;
+        Sticky.prototype.updatePosition = function () {
+            var fixedTop, css;
 
-        this.read && fastdom.clear(this.read);
-        this.read = fastdom.read(function () {
-            // have we scrolled past the element
-            if (window.scrollY >= this.$parent.offset().top - this.opts.top) {
-                // make sure the element stays within its parent
-                fixedTop = Math.min(this.opts.top, this.$parent[0].getBoundingClientRect().bottom - this.$element.dim().height);
+            this.read && fastdom.clear(this.read);
+            this.read = fastdom.read(function () {
+                // have we scrolled past the element
+                if (window.scrollY >= this.$parent.offset().top - this.opts.top) {
+                    // make sure the element stays within its parent
+                    fixedTop = Math.min(this.opts.top, this.$parent[0].getBoundingClientRect().bottom - this.$element.dim().height);
 
-                css = {
-                    position: 'fixed',
-                    top:      fixedTop
-                };
-            } else {
-                css = {
-                    position: null,
-                    top:      null
-                };
-            }
+                    css = {
+                        position: 'fixed',
+                        top:      fixedTop
+                    };
+                } else {
+                    css = {
+                        position: null,
+                        top:      null
+                    };
+                }
 
-            this.write && fastdom.clear(this.write);
-            this.write = fastdom.write(function () {
-                this.$element.css(css);
+                this.write && fastdom.clear(this.write);
+                this.write = fastdom.write(function () {
+                    this.$element.css(css);
+                }.bind(this));
             }.bind(this));
-        }.bind(this));
-    };
+        };
+    }
 
     return Sticky;
 });
